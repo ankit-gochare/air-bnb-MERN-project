343eafc22fb47b47dc78ab48a97b06821be5d43a
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-05 13:06:16 +0530

    index route implemented

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express')
 const listtingRouter = require('./routes/listing.routes')
+const path = require('path')
 
 const app = express();
 
-app.use("/api/listings" , listtingRouter)
+app.set("view engine" , "ejs")
+app.set("views" , path.join(__dirname, "views"));
+
+
+app.use("/" , listtingRouter)
 
 module.exports = app
--- src/routes/listing.routes.js
+++ src/routes/listing.routes.js
@@ -3,6 +3,7 @@ const listingModel = require('../models/listing.model')
 
 const listingRouter = express.Router()
 
+// listingModel testing route
 // listingRouter.get("/testListing" , async (req,res)=>{
 //     let sampleListing = new listingModel({
 //         title:"Test Villa",
@@ -16,5 +17,12 @@ const listingRouter = express.Router()
 //     console.log("sample was saved")
 //     res.send("successful testing")
 // })
+
+// get all listings route
+listingRouter.get("/listings" , async(req,res)=>{
+    const allListings = await listingModel.find({})
+
+    res.render("listings/index.ejs" , {allListings});
+})
 
 module.exports = listingRouter
