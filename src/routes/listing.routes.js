54665e1256c19d0eeebdffa481edbb92ad130bcc
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-05 13:46:26 +0530

    create new listing implemented

--- src/routes/listing.routes.js
+++ src/routes/listing.routes.js
@@ -25,6 +25,24 @@ listingRouter.get("/listings" , async(req,res)=>{
     res.render("listings/index.ejs" , {allListings});
 })
 
+// new route
+listingRouter.get("/listings/new" , (req,res)=>{
+    res.render("listings/new.ejs")
+})
+// is route ko niche rkha to :id 
+// new ko bhi id smjhega
+
+// create route
+listingRouter.post("/listings", async (req,res)=>{
+    // let listing = req.body.listing;
+    // console.log(listing);
+    const newListing = new listingModel(req.body.listing);
+
+    await newListing.save();
+
+    res.redirect("/listings");
+})
+
 // show route // individual listings
 listingRouter.get("/listings/:id" , async(req,res)=>{
     let {id} = req.params;
@@ -32,5 +50,7 @@ listingRouter.get("/listings/:id" , async(req,res)=>{
 
     res.render("listings/show.ejs" , {listing});
 })
+
+
 
 module.exports = listingRouter
