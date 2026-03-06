c14e53502a0f6696b99085d1b6367fc69956f6b5
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-06 10:57:40 +0530

    Navbar added

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ const app = express();
 app.use(methodOverride("_method"));
 app.set("view engine" , "ejs")
 app.set("views" , path.join(__dirname, "views"));
-
 app.use(express.urlencoded({extended:true}));
 app.engine("ejs" , ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use("/" , listtingRouter)
 
